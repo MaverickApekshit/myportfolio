53d5279b62896b57133ad9f6494b3d6c7044cad1
Apekshit <apekshit33@gmail.com>
2019-11-12 21:04:54 +0530

    Added static home page

--- myportfolio/urls.py
+++ myportfolio/urls.py
@@ -17,10 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-import projects.views
+import resume.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', projects.views.home, name='home'),
+    path('', resume.views.home, name='home'),
     path('blog/', include('blog.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
