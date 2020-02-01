1785d6cb2b3f55bf31fa2c1d7f9e93c47be9e056
Apekshit <apekshit33@gmail.com>
2020-02-01 21:33:26 +0530

    Added projects section

--- projects/models.py
+++ projects/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+import datetime
 
 class Project(models.Model):
-	"""Model for rendering project overviews"""
+	"""Model for rendering project"""
 	
 	image = models.ImageField(upload_to='images/')
-	summary = models.CharField(max_length=200)
+	tile = models.CharField(max_length=100, default="Project Name")
+	tech_stack = models.CharField(max_length=200)
+	start_date = models.DateField(default=datetime.date.today)
+
+	class Meta:
+		ordering = ["-start_date"]
--- resume/views.py
+++ resume/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Job
+from projects.models import Project
 
 def home(request):
 	jobs = Job.objects
-	return render(request, 'resume/home.html', {'jobs':jobs})
+	projects = Project.objects
+	return render(request, 'resume/home.html', {'jobs':jobs, 'projects':projects})
