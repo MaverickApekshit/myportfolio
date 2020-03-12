98ba2bccc1c4a6c670808692cc2b8bc7ab111af9
Apekshit <apekshit33@gmail.com>
2020-03-12 22:29:42 +0530

    Updated project model with description fields

--- projects/models.py
+++ projects/models.py
@@ -7,7 +7,10 @@ class Project(models.Model):
 	image = models.ImageField(upload_to='images/')
 	title = models.CharField(max_length=100, default="Project Name")
 	tech_stack = models.CharField(max_length=200)
+	github_link = models.URLField(max_length=500, blank=True)
+	demo_link = models.URLField(max_length=500, blank=True)
 	start_date = models.DateField(default=datetime.date.today)
+	discription = models.TextField(default="Project Description")
 
 	class Meta:
 		ordering = ["-start_date"]
